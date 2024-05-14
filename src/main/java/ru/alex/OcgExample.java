1ffb3ae9d5a184e501c762da7dc2ef484b9d7204
alex-from-90 <alex@site-n.ru>
2024-05-14 13:39:54 +0300

    add PDF layer to file

---
+++ src/main/java/ru/alex/OcgExample.java
@@ -0,0 +1,42 @@
+package ru.alex;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.graphics.optionalcontent.PDOptionalContentProperties;
+import org.apache.pdfbox.rendering.PDFRenderer;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+
+public class OcgExample {
+    public static void main(String[] args) {
+        String inputFile = "src/main/resources/doc.pdf";
+        try (PDDocument document = PDDocument.load(new File(inputFile))) {
+            PDOptionalContentProperties ocProps = document.getDocumentCatalog().getOCProperties();
+            if (ocProps != null) {
+                PDFRenderer renderer = new PDFRenderer(document);
+                for (String layerName : ocProps.getGroupNames()) {
+                    // Для каждого слоя отключаем все слои...
+                    for (String ln : ocProps.getGroupNames()) {
+                        ocProps.setGroupEnabled(ln, false);
+                    }
+                    // ... и включаем только текущий
+                    ocProps.setGroupEnabled(layerName, true);
+                    // Рендеринг страницы с учетом установленной видимости слоёв
+                    for (int i = 0; i < document.getNumberOfPages(); ++i) {
+                        BufferedImage image = renderer.renderImageWithDPI(i, 300);
+                        String outputFileName = layerName + "_page_" + (i + 1) + ".png";
+                        // Сохранение рендеренной страницы в файл
+                        ImageIO.write(image, "PNG", new File(outputFileName));
+                    }
+                    System.out.println("Слой " + layerName + " был обработан и сохранен.");
+                }
+            } else {
+                System.out.println("Документ не содержит слоев (OCG).");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
